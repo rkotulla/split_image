1229009056bede3bf165d2e4f35d2ded107ff9d0
Ralf Kotulla <ralf.kotulla@gmail.com>
2016-11-04 08:21:15 -0500

    added more stdout output for user feedback.

--- split_image.py
+++ split_image.py
@@ -12,6 +12,7 @@ if __name__ == "__main__":
 
     fn = sys.argv[1]
     if (not os.path.isfile(fn)):
+        print("Could not find input file %s" % (fn))
         sys.exit(0)
 
     nx = int(sys.argv[2])
@@ -29,14 +30,16 @@ if __name__ == "__main__":
 
     for x,y in itertools.product(range(nx), range(ny)):
 
+        out_fn = "%s.x%02dy%02d.fits" % (fn[:-5], x, y)
+
         start_x = numpy.max([int(math.floor(x * sx - overlap)), 0])
         end_x = numpy.min([int(math.ceil((x+1)*sx + overlap)), img.shape[1]])
 
         start_y = numpy.max([int(math.floor(y * sy - overlap)), 0])
         end_y = numpy.min([int(math.ceil((y+1)*sy + overlap)), img.shape[0]])
 
-        print("Creating cutout x=%02d, y=%02d (x: %5d--%5d, y: %5d--%5d" % (
-            x,y,start_x, end_x, start_y, end_y)
+        print("Creating cutout x=%02d, y=%02d (x: %5d--%5d, y: %5d--%5d) --> %s" % (
+            x,y,start_x, end_x, start_y, end_y, out_fn)
               )
         cutout = img[start_y:end_y, start_x:end_x]
 
@@ -44,7 +47,6 @@ if __name__ == "__main__":
         phdu.header['CRPIX1'] -= start_x
         phdu.header['CRPIX2'] -= start_y
 
-        out_fn = "%s.x%02dy%02d.fits" % (fn[:-5], x, y)
         # print("Writing cutout to %s" % (out_fn))
         phdu.writeto(out_fn, clobber=True)
 
